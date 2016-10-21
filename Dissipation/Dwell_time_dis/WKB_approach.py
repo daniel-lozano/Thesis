d0632a15757fee633b52c66017371dcc99162a26
daniel-lozano <d.lozano343@uniandes.edu.co>
2016-10-20 20:30:58 -0500

    actualizado

--- Dissipation/Dwell_time_dis/WKB_approach.py
+++ Dissipation/Dwell_time_dis/WKB_approach.py
@@ -7,17 +7,23 @@ import matplotlib.pyplot as plt
 %%%%%%%%%%%%%%%%%%%% Funciones utilizadas en el calculo del tiemo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 '''
 
-def k(x,g,Vo,m,ep):#sheck 
-    return g*np.sqrt(2*ep*Vo(E-1)/m)-((g*x)**2)/(2*m)
+def k(m,eta,Vo,E,hbar,ep):#sheck
+    
+    k0=np.sqrt(2*m*ep*Vo*(E-1))/hbar
+    
+    return np.sqrt(k0**2-(ep*2*eta*k0*x)/hbar +(eta*x/hbar)**2 )
 
-
-def constant(a,b,c,x,m,hbar):
-    return -2*((np.sqrt(2m)/(8*(a**1.5)*hbar))*( 2*np.sqrt(a)*(2*a*x+b)*np.sqrt((a*x+b)*x +c )-(b**2-4*a*c)*np.log(2*np.sqrt(a))*np.sqrt((a*x+b)*x +c )+ 2*a*x+b  )
-
-               
-def function(a,b,c,x,m,hbar):
-     return -2*((np.sqrt(2m)/(8*(a**1.5)*hbar))*( 2*np.sqrt(a)*(2*a*x+b)*np.sqrt((a*x+b)*x +c )-(b**2-4*a*c)*np.log(2*np.sqrt(a))*np.sqrt((a*x+b)*x +c )+ 2*a*x+b)
-                
+def function(m,eta,Vo,E,hbar,ep):
+    
+    k0=np.sqrt(2*m*ep*Vo*(E-1))/hbar
+    
+    termino1=-0.5*(2*x-ep*hbar*k0/eta)
+    
+    termino2=np.sqrt(k0**2 -(ep*2*eta*k0*x)/hbar +(eta*x/hbar)**2   )
+    
+    termino3= (1/eta)*(np.sqrt(2)*hbar*k0**2)*(1-ep)**1.5
+    
+    return np.exp(-0.5*(termino1*termino2+termino3))
 
 '''
 %%%%%%%%%%%%%%%%%%%% Variables usadas en el calculo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -26,22 +32,21 @@ def function(a,b,c,x,m,hbar):
 
 #definiendo el espacio
 
-a=0.5*(20.8)E5#fermi
-Nx=1000
-x=np.linspace(-a,a,Nx)
-dx=abs(x[0]-x[1])
 
 #definiendo energias adimensionales
 Ne=100
 E=np.linspace(0,2,Ne)
 
-gamma=[0,1E-3,1E-5]# eV/Fermi
+eta=[1E-7,1E-5,1E-7]# eV/Fermi
 
 #Constantes usadas
 
-Vo=1.8E-0.6 #MeV
+
+Vo=1.8E-6
 m=0.5E6# MeV
-hbar=179.3# MeV*fermi
+hbar=197.327# MeV*fermi hbar*c
+
+
 
 #matriz que traza el cambio entre E/Vo
 epsilon=np.zeros(len(E))
@@ -57,9 +62,38 @@ for i in range(Ne):
 '''
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Comenzando el calculo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 '''
+a=20.8E5#fermi
+Nx=1000
 
+                
 
 for i in range(len(E)):
+    
+    #defining the array for the intergration
+    
+    tpoint=np.sqrt(2*m*epsilon[i]*Vo*(E[i]-1))/eta[0]
+    x=np.linspace(tpoint,a,Nx)
+    dx=abs(x[0]-x[1])
+    
+    #suma
+    suma=0
+    
+    expo=function(m,eta[0],Vo,E[i],hbar,epsilon[i])
+        
+    Kfunc=k(m,eta[0],Vo,E[i],hbar,epsilon[i])
+    
+    #se empieza la integral
+    for j in range(len(x)):
+        
+        suma+=dx*(1/Kfunc[j])*expo[j]
+    
+    #este es el tiempo hallado
+    T[i]=suma
+
+
+        
+
+        
 
 
 
