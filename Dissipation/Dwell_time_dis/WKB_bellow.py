abfd34828bf6e8ec2a553854c994b3588a4f4091
daniel-lozano <d.lozano343@uniandes.edu.co>
2016-11-01 12:59:28 -0500

    corrected

--- Dissipation/Dwell_time_dis/WKB_bellow.py
+++ Dissipation/Dwell_time_dis/WKB_bellow.py
@@ -9,9 +9,16 @@ from scipy import integrate
 '''
 
 def integrand(x,k,eta,hbar,m,a):
-    exp=np.exp(-2*k*x*(1+eta*x/2))
-    div=1+eta*x
-    return exp/div
+    
+    factor1= - 2*k*a*x
+
+    factor2= - (eta/hbar)*pow(x*a,2)
+
+    exp= np.exp(factor1+factor2)
+
+    div= hbar*k + eta*x*a
+
+    return a*m*exp/div
 
 '''
 %%%%%%%%%%%%%%%%%%%% Variables usadas en el calculo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -23,13 +30,13 @@ def integrand(x,k,eta,hbar,m,a):
 
 #definiendo energias adimensionales
 Ne=100
-E=np.linspace(0.001,0.99,Ne)
+E=np.linspace(0.01,0.99,Ne)
 
 #Constantes usadas
 
-a=20.8E5#fermi
-Vo=1.8E-6
-m=0.5# MeV
+a=20.7989E5  #fermi
+Vo=1.8E-6 # MeV
+m=0.51     # MeV
 hbar=197.327# MeV*fermi =[hbar*c]
 
 print("\nmass=",m, " MeV")
@@ -42,11 +49,8 @@ print("hbar=",hbar," Mev*fermi\n")
 Tenemos que tener que eta*l << hbar*k, para satisfacerlo tomaremos el k mas pequeño posible correspondiente a E=0
 '''
 
-hbar_k_l=np.sqrt(2*m*Vo*(1-E[-1]))/a
 
-print("Condition:\neta <<",hbar_k_l)
-
-eta=np.linspace(0,0.1E-8,10)/hbar_k_l # eV/Fermi
+eta=np.linspace(0,5E-11,2) # eV/Fermi
 
 print("eta=",eta)
 
@@ -65,24 +69,36 @@ for j in range(len(eta)):
     for i in range(len(E)):
     
         k=np.sqrt(2*m*Vo*(1-E[i]))/hbar
+        
         kp=np.sqrt(2*m*Vo*(E[i]))/hbar
         
-        N_factor=2*np.sqrt(k*kp/(k**2+kp**2))
+        N_factor= 2*np.sqrt(k*kp/(k**2+kp**2))
     
         funcion= lambda x: integrand(x,k,eta[j],hbar,m,a)
     
-        resultados=integrate.quad(funcion,0,1,limit=100)
+        resultados=integrate.quad(funcion,0,1,limit=100,limlst=96)
 
-        T[i]=(N_factor**1)*(6.58*10**(-16))*(m*a/(hbar*k))*(resultados[0])/hbar
-        nombre="$ \hat{\eta}= $"+str(eta[j])
+        T[i]=(N_factor**2)*(resultados[0])* (6.58*10**(-22)/hbar)
+        
+        nombre="$ \eta= $"+str(eta[j]) +"$  MeV $"
 
         
     plt.plot(E,T,label=nombre)
+
+for i in range(len(T)):
+    
+    print(E[i],T[i])
+
+
+
+
+
 
 #plt.figure(figsize=(20,10))
 
 plt.legend(loc=2)
 plt.xlabel("$  E/V_0 $",size=20)
+plt.ylim(0,1E-15)
 plt.ylabel(" $ Time $ ",size=20)
 plt.savefig("Integral_disp_vel.png")
 
